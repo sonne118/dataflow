82c792601c36f6726b3455d61589dc992e962c0f
sonne <_sonne_@ukr.net>
2023-03-12 20:07:23 +0200

    Add main functionality

---
+++ Actor/Actor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actor
+{
+    public class ParserActor : AbstractActor<string>
+    {
+        private readonly HabrParser _parser;
+        private readonly EventBus _eventBus;
+
+        protected override int ThreadCount => 10;
+
+        public ParserActor(EventBus eventBus)
+        {
+            _parser = new HabrParser();
+            _eventBus = eventBus;
+        }
+
+        protected override async Task HandleItem(string message)
+        {
+            try
+            {
+                var topics = await _parser.ParseTopics(message);
+
+                foreach (var item in topics)
+                {
+                    _ = _eventBus.Publish(new OnTopicParsed(item));
+                }
+            }
+            catch (Exception ex)
+            {
+                await SendMessage(message);
+            }
+        }
+
+    }
+
+
+    public class OnTopicParsed : IEvent
+    {
+        public OnTopicParsed(Topic topic)
+        {
+            Topic = topic;
+        }
+
+        public Topic Topic { get; set; }
+    }
+
+}
---
+++ Actor/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Actor
+{
+    class Program
+    {
+        private static ParserActor actor;
+        private static SumActor sumActor;
+        private static EventBus eventbus;
+
+        static async Task Main(string[] args)
+        {
+            eventbus = new EventBus();
+            actor = new ParserActor(eventbus);
+            sumActor = new SumActor();
+
+            eventbus.Subscribe<OnTopicParsed>(HandleOnTopicParsed);
+
+
+            await sumActor.SendMessage(1);
+            await sumActor.SendMessage(1);
+
+
+            await Task.Delay(1);
+            var state = sumActor.GetState();
+
+            await DoParse();
+
+            Console.ReadLine();
+        }
+
+        private static Task HandleOnTopicParsed(OnTopicParsed arg)
+        {
+            Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Parsed: {arg.Topic.Title} - {arg.Topic.Url}");
+
+            return Task.CompletedTask;
+        }
+
+        private static async Task DoParse()
+        {
+            var parser = new HabrParser();
+
+            await foreach (var url in parser.GetPages())
+            {
+                await actor.SendMessage(url);
+            }
+        }
+
+    }
+
+
+}
+
+
